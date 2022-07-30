adaab6ead4b733088002bc8b3824f7aede03304f
linjinhui <linjinhui@goodix.com>
2022-07-30 16:40:43 +0800

    兼容 /usr/src/linux-headers-4.9.253-tegra-ubuntu18.04_aarch64/kernel-4.9 老内核版本

--- 08_timer/mytimer.c
+++ 08_timer/mytimer.c
@@ -1,8 +1,13 @@
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/gpio.h>
+// #include <linux/gpio.h>
 #include <linux/jiffies.h>
 #include <linux/timer.h>
+#include <linux/timekeeping.h>
+
+// https://blog.csdn.net/qq_26558047/article/details/115249587
+// https://gist.github.com/itrobotics/596443150c01ff54658e
+// https://www.kernel.org/doc/html/latest/core-api/timekeeping.html
 
 /* Meta Information */
 MODULE_LICENSE("GPL");
@@ -12,8 +17,21 @@ MODULE_DESCRIPTION("A simple test for LKM's timer");
 /** variable for timer */
 static struct timer_list my_timer;
 
-void timer_callback(struct timer_list * data) {
-	gpio_set_value(4, 0); /* Turn LED off */
+static int times = 0;
+static u64 last_ns = 0;
+static u64 current_ns = 0;
+
+static void timer_callback(unsigned long data) {
+	times ++;
+	printk("timer callback : %d\n", times);
+	last_ns = current_ns;
+	current_ns = ktime_get_ns();
+	printk("current ns : %lld\n", current_ns);
+	printk("current ns diff : %lld\n", current_ns - last_ns);
+	// gpio_set_value(4, 0); /* Turn LED off */
+
+	// modify the timer for next time
+	mod_timer(&my_timer, jiffies + HZ / 2);
 }
 
 /**
@@ -21,26 +39,31 @@ void timer_callback(struct timer_list * data) {
  */
 static int __init ModuleInit(void) {
 	printk("Hello, Kernel!\n");
-	/* GPIO 4 init */
-	if(gpio_request(4, "rpi-gpio-4")) {
-		printk("Can not allocate GPIO 4\n");
-		return -1;
-	}
+	// /* GPIO 4 init */
+	// if(gpio_request(4, "rpi-gpio-4")) {
+	// 	printk("Can not allocate GPIO 4\n");
+	// 	return -1;
+	// }
 
-	/* Set GPIO 4 direction */
-	if(gpio_direction_output(4, 0)) {
-		printk("Can not set GPIO 4 to output!\n");
-		gpio_free(4);
-		return -1;
-	}
+	// /* Set GPIO 4 direction */
+	// if(gpio_direction_output(4, 0)) {
+	// 	printk("Can not set GPIO 4 to output!\n");
+	// 	gpio_free(4);
+	// 	return -1;
+	// }
 
 	/* Turn LED on */
-	gpio_set_value(4, 1);
+	// gpio_set_value(4, 1);
 
 	/* Initialize timer */
-	timer_setup(&my_timer, timer_callback, 0);
-	mod_timer(&my_timer, jiffies + msecs_to_jiffies(1000));
+	// setup_timer(&my_timer, timer_callback, 0);
 
+	init_timer(&my_timer);
+	my_timer.expires = jiffies + HZ ;
+    my_timer.data = (unsigned long) NULL;
+    my_timer.function = timer_callback;
+    /* ... */
+    add_timer(&my_timer);
 
 	return 0;
 }
@@ -50,7 +73,7 @@ static int __init ModuleInit(void) {
  */
 static void __exit ModuleExit(void) {
 	printk("Goodbye, Kernel\n");
-	gpio_free(4);
+	// gpio_free(4);
 	del_timer(&my_timer);
 }
 
